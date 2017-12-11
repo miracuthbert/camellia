7b665d1e0873e79b983494975ab7528092e872c5
miracuthbert <miracuthbert@gmail.com>
2017-12-11 16:28:46 +0300

    Update menu view
    Updated item listing
    Updated add to cart functionality

--- orders/menu.php
+++ orders/menu.php
@@ -21,58 +21,69 @@ $foods = foods();
         <div class="row">
             <div class="col-sm-12">
 
-                <form class="form-horizontal" method="POST" action="<?php echo route('orders/scripts/calculate_cart.php'); ?>"
-                      enctype="application/x-www-form-urlencoded">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3><?php echo APP_NAME; ?> Menu</h3>
-                            <p>Choose meal(s) and (or) beverage(s) from below:</p>
-                        </div>
+                <h3><?php echo APP_NAME; ?> Menu</h3>
+                <p>Choose meal(s) and (or) beverage(s) from below:</p>
 
-                        <div class="panel-body">
-                            <?php foreach ($foods as $food) { ?>
+                <div class="row">
+                    <?php foreach ($foods as $food) { ?>
+                        <div class="col-sm-4">
+                            <form id="food-<?php echo $food['id']; ?>" class="text-center" method="POST"
+                                  action="<?php echo route('orders/scripts/add_to_cart.php'); ?>"
+                                  enctype="application/x-www-form-urlencoded">
+                                <input type="hidden" name="order" value="<?php echo $food['id']; ?>">
+
+                                <img class="media-object" src="<?php echo $food['image']; ?>" alt="image"
+                                     class="img-responsive">
+
                                 <div class="form-group">
-                                    <div class="col-sm-2">
-                                        <input type="checkbox" name="order[]" id="food-<?php echo $food['id']; ?>"
-                                               value="<?php echo $food['id']; ?>">
+                                    <h4 data-toggle="tooltip" title="<?php echo $food['details']; ?>">
+                                        <?php echo $food['name']; ?>
+                                        <i class="fa fa-info-circle"></i>
+                                    </h4>
+                                </div>
 
-                                        <img class="media-object" src="<?php echo $food['image']; ?>" alt="image"
-                                             class="img-responsive">
-                                    </div>
-                                    <div class="col-sm-8">
+                                <div class="form-group">
+                                    <?php echo APP_CURRENCY; ?> <span class="lead"><?php echo $food['price']; ?></span>
+                                </div>
 
-                                        <h4 data-toggle="tooltip" title="<?php echo $food['details']; ?>">
-                                            <?php echo $food['name']; ?>
-                                            <i class="fa fa-info"></i>
-                                        </h4>
-                                        <strong><?php echo APP_CURRENCY; ?><?php echo $food['price']; ?></strong>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="input-group">
-                                            <span class="input-group-addon">Qty.</span>
-                                            <input type="number" name="order_quantity[]" class="form-control"
-                                                   max="20"
-                                                   value="1">
-                                        </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <span class="input-group-addon">Qty.</span>
+
+                                        <input type="number" name="order_quantity"
+                                               class="form-control order-quantity"
+                                               data-target="#food-<?php echo $food['id']; ?>" max="20" min="1"
+                                               value="1">
+
+                                        <span class="input-group-btn">
+                                       <button type="submit" class="btn btn-success pull-right">
+                                            <i class="fa fa-cart-plus"></i> Add to Cart
+                                        </button>
+                                    </span>
                                     </div>
                                 </div>
-                            <?php } ?>
+                            </form>
                         </div>
-
-                        <div class="panel-footer">
-                            <div class="clearfix">
-                                <button type="submit" class="btn btn-success pull-right">
-                                    <i class="fa fa-cart-plus"></i> Add to Cart
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <?php include_once('../partials/_scripts.php') ?>
+<script>
+    $(document).on('order-quantity', 'focus', (event)
+    {
+        var $this = $(this);
+
+        var food = $this.attr('data-target');
+
+        //check food of focused quanntity
+        $(food).prop('checked', true);
+    }
+    )
+    ;
+</script>
 </body>
 </html>
