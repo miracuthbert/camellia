8383c6ffd30d2051656655d3d157abdf7be59197
miracuthbert <miracuthbert@gmail.com>
2017-12-11 09:45:11 +0300

    Add empty cart script
    Removes/clears cart session

---
+++ orders/scripts/empty_cart.php
@@ -0,0 +1,24 @@
+<?php
+
+    include_once "../../config.php";
+    include_once "../../helpers.php";
+    include_once "../../functions.php";
+
+    if (isset($_POST)) {
+
+        //check if cart exists and remove it
+        if (isset($_SESSION['cart'])) {
+
+            //remove cart from session
+            unset($_SESSION['cart']);
+
+            $_SESSION['success'] = "Your cart has been emptied. You can start adding new items again.";
+
+            return header("Location: " . route("orders/menu.php"));
+
+        }
+    }
+
+    $_SESSION['error'] = "Invalid request. Please try again.";
+
+    return header("Location: " . route("orders/cart.php"));
