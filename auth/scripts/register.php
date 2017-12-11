342579ceb4ae68c246c779578912c1bbd021cb86
miracuthbert <miracuthbert@gmail.com>
2017-12-11 17:41:53 +0300

    Update register script
    Fix last name post fetch

--- auth/scripts/register.php
+++ auth/scripts/register.php
@@ -11,7 +11,7 @@
 
         //validate data
         $first_name = $_POST['first_name'];
-        $last_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
         $password = $_POST['password'];
