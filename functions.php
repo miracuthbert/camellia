558b7419431db3870dca0cd00e5fae49ae68fb8e
miracuthbert <miracuthbert@gmail.com>
2017-12-11 16:32:58 +0300

    Update functions
    Comment out orderFoods function
    Update cartTotal function

--- functions.php
+++ functions.php
@@ -125,61 +125,62 @@ if (!function_exists('foods')) {
         return $rows;
     }
 }
-if (!function_exists('orderFoods')) {
-    /**
-     * Fetch foods.
-     */
-    function orderFoods()
-    {
 
-        global $con;
-
-        $true = true;
-
-        if (!isset($_SESSION['cart']['orders'])) {
-            $_SESSION['error'] = "Please add at least one item to cart, before checkout";
-
-            return header("Location: " . route("orders/menu.php"));
-        }
-
-        $fullArr = $_SESSION['cart']['orders']; //merge `parent` in WHERE IN array with other value(s)
-        $clause = implode(',', array_fill(0, count($fullArr), '?')); //create `x` question marks
-
-        array_push($fullArr, $true);
-
-        $types = str_repeat('i', (count($fullArr))); //create `x` ints for bind_param
-
-
-        //find foods
-        $stmt = $con->prepare("SELECT 
-                                      `foods`.*, 
-                                      `categories`.`id` AS `categoryId`, 
-                                      `categories`.`name` AS `categoryName`, 
-                                      `categories`.`slug` AS `categorySlug` 
-                                        FROM `foods` 
-                                          INNER JOIN 
-                                            `categories` 
-                                              ON `foods`.`category_id` = `categories`.`id` 
-                                      WHERE `foods`.`id` IN ($clause)
-                                        AND `foods`.`usable` = ? 
-                                        AND `foods`.`usable` IS NOT NULL 
-                                        AND `foods`.`category_id` IS NOT NULL");
-        $stmt->bind_param($types, ...$fullArr);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows === 0) {
-            $_SESSION['error'] = "Please add at least one item to cart, before checkout";
-
-            return header("Location: " . route("orders/menu.php"));
-        };
-        $rows = $result->fetch_all(MYSQLI_BOTH);
-
-        $stmt->close();
-
-        return $rows;
-    }
-}
+//if (!function_exists('orderFoods')) {
+//    /**
+//     * Fetch foods.
+//     */
+//    function orderFoods()
+//    {
+//
+//        global $con;
+//
+//        $true = true;
+//
+//        if (!isset($_SESSION['cart']['items'])) {
+//            $_SESSION['error'] = "Please add at least one item to cart, before checkout";
+//
+//            return header("Location: " . route("orders/menu.php"));
+//        }
+//
+//        $fullArr = array_keys($_SESSION['cart']['items'], 'id'); //merge `parent` in WHERE IN array with other value(s)
+//        $clause = implode(',', array_fill(0, count($fullArr), '?')); //create `x` question marks
+//
+//        array_push($fullArr, $true);
+//
+//        $types = str_repeat('i', (count($fullArr))); //create `x` ints for bind_param
+//
+//
+//        //find foods
+//        $stmt = $con->prepare("SELECT
+//                                      `foods`.*,
+//                                      `categories`.`id` AS `categoryId`,
+//                                      `categories`.`name` AS `categoryName`,
+//                                      `categories`.`slug` AS `categorySlug`
+//                                        FROM `foods`
+//                                          INNER JOIN
+//                                            `categories`
+//                                              ON `foods`.`category_id` = `categories`.`id`
+//                                      WHERE `foods`.`id` IN ($clause)
+//                                        AND `foods`.`usable` = ?
+//                                        AND `foods`.`usable` IS NOT NULL
+//                                        AND `foods`.`category_id` IS NOT NULL");
+//        $stmt->bind_param($types, ...$fullArr);
+//        $stmt->execute();
+//        $result = $stmt->get_result();
+//
+//        if ($result->num_rows === 0) {
+//            $_SESSION['error'] = "Please add at least one item to cart, before checkout";
+//
+//            return header("Location: " . route("orders/menu.php"));
+//        };
+//        $rows = $result->fetch_all(MYSQLI_BOTH);
+//
+//        $stmt->close();
+//
+//        return $rows;
+//    }
+//}
 
 if (!function_exists('cartTotal')) {
     /**
@@ -189,9 +190,9 @@ if (!function_exists('cartTotal')) {
     {
         $total = 0;
 
-        if (isset($_SESSION['cart']['orders']) && isset($_SESSION['cart']['orderPrices'])) {
-            foreach ($_SESSION['cart']['orderPrices'] as $orderPrice) {
-                $total += $orderPrice;
+        if (isset($_SESSION['cart']['items']) && count($_SESSION['cart']['items'])) {
+            foreach ($_SESSION['cart']['items'] as $item) {
+                $total += $item['totalPrice'];
             }
         }
 
