806f14d8b8deaf51498c02efc4f1c79fc9e8c61a
miracuthbert <miracuthbert@gmail.com>
2017-12-11 09:47:45 +0300

    Update scripts partial
    Added code to unset/clear bulk errors if in session

--- partials/_scripts.php
+++ partials/_scripts.php
@@ -1,2 +1,9 @@
 <!-- Scripts -->
 <script src="<?php echo APP_URL . "/public/js/app.js"; ?>"></script>
+
+<?php
+
+//unset session errors if exists
+if (isset($_SESSION['errors'])) {
+    unset($_SESSION['errors']);
+}
