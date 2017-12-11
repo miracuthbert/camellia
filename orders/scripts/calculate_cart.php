20d7ad4a6efad99ef5ce84fa3fc38edb52049359
miracuthbert <miracuthbert@gmail.com>
2017-12-11 16:36:07 +0300

    Update calculate_cart script
    Updated script to a callable function

--- orders/scripts/calculate_cart.php
+++ orders/scripts/calculate_cart.php
@@ -1,50 +1,23 @@
 <?php
 
-    include_once "../../config.php";
-    include_once "../../helpers.php";
-    include_once "../../functions.php";
+function calculateCart($orders, $orderQtys)
+{
 
-    if (isset($_POST)) {
-        $_SESSION['old'] = $_POST;
+    //check if no items are selected then redirect back
+    if (!isset($orders) || !isset($orderQtys)) {
+        $_SESSION['error'] = "Add at least one item to cart.";
 
-        //catch orders
-        $orders = $_POST['order'];
-        $orderItems = $_POST['order_quantity'];
-        $foodPrices = [];
-        $orderPrices = [];
-
-        //check if no items are selected then redirect back
-        if(!isset($orders) || !isset($orderItems)) {
-            $_SESSION['error'] = "Add at least one item to cart.";
-
-            return header("Location: " . route("orders/menu.php"));
-        }
-
-        foreach ($orders as $key => $order) {
-            //find food
-            $food = food($order);
-
-            //push to food prices
-            array_push($foodPrices, $food['price']);
-
-            //calculate total price
-            $price = $food['price'] * $orderItems[$key];
-
-            //push to order price
-            array_push($orderPrices, $price);
-
-//            echo "{$key} . {$order} | {$orderItems[$key]} * {$food['price']} = ({$price}) .<br/>";
-        }
-
-        //create cart
-        $_SESSION['cart']['orders'] = $orders;
-        $_SESSION['cart']['orderQty'] = $orderItems;
-        $_SESSION['cart']['orderPrices'] = $orderPrices;
-        $_SESSION['cart']['foodPrices'] = $foodPrices;
-
-        return header("Location: " . route("orders/cart.php"));
+        return header("Location: " . route("orders/menu.php"));
     }
 
-    $_SESSION['error'] = "Please add at least one item to cart first before proceeding to cart.";
+    foreach ($orders as $key => $order) {
+        $item = $_SESSION['cart']['items'][$order];
 
-    header("Location: " . route("orders/menu.php"));
+        $_SESSION['cart']['items'][$order]['qty'] = $orderQtys[$key];
+        $_SESSION['cart']['items'][$order]['totalPrice'] = $item['price'] * $orderQtys[$key];
+    }
+
+    $_SESSION['success'] = "Cart updated successfully.";
+
+    return header("Location: " . route("orders/cart.php"));
+}
