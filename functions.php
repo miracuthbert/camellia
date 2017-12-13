90325352e288b8efccd8322fd0b2c718f5acf5d8
miracuthbert <miracuthbert@gmail.com>
2017-12-13 18:10:24 +0300

    Update functions.php
    Move order related (order, orderPayment, OrderItems) functions from Admin functions to functions.php

--- functions.php
+++ functions.php
@@ -463,3 +463,119 @@ if (!function_exists('userOrders')) {
         return $rows;
     }
 }
+
+
+if (!function_exists('order')) {
+    /**
+     * Fetch passed order.
+     *
+     * @param $order
+     * @return array|void
+     */
+    function order($order)
+    {
+
+        global $con;
+
+        $row = [];
+
+        if (!isset($order)) {
+
+            //set success message
+            $_SESSION['error'] = "Page not found.";
+
+            //back to register with error
+            return header("Location: " . route('admin/orders/index.php'));
+
+        }
+
+        $stmt = $con->prepare("SELECT * FROM `orders` WHERE `id` = ? LIMIT 1");
+        $stmt->bind_param("i", $order);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+        } else {
+            die('Page not found.');
+        };
+
+        $stmt->close();
+
+        return $row;
+    }
+}
+
+if (!function_exists('orderPayment')) {
+    /**
+     * Fetch passed order payment details.
+     *
+     * @param $order
+     * @return array
+     */
+    function orderPayment($order)
+    {
+
+        global $con;
+
+        $row = [];
+
+        if (!isset($order)) {
+            return $row;
+        }
+
+        $stmt = $con->prepare("SELECT * FROM `order_payments` WHERE `order_id` = ? LIMIT 1");
+        $stmt->bind_param("i", $order);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+        }
+
+        $stmt->close();
+
+        return $row;
+    }
+}
+
+if (!function_exists('orderItems')) {
+    /**
+     * Fetch passed order items.
+     */
+    function orderItems($order)
+    {
+
+        global $con;
+
+        $rows = [];
+
+        if (!isset($order)) {
+
+            return $rows;
+        }
+
+        $stmt = $con->prepare("SELECT
+                                        `order_items`.*,
+                                        `foods`.`name`,
+                                        `foods`.`details`,
+                                        `foods`.`image`
+                                    FROM
+                                        `order_items`
+                                    INNER JOIN `foods` 
+                                    WHERE 
+                                    `order_items`.`food_id` = `foods`.`id` 
+                                    AND `order_id` = ?");
+        $stmt->bind_param("s", $order);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows >= 1) {
+            $rows = $result->fetch_all(MYSQLI_BOTH);
+        }
+
+        $stmt->close();
+
+        return $rows;
+    }
+}
